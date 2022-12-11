fc90f5a7017c0ea675fe430f14dfb3439493832f
zlxlty <lus2@carleton.edu>
2022-12-11 16:43:43 +0100

    add image to detail info dialog

--- app/src/main/java/com/example/donatr/summary/MoreInfoDialog.kt
+++ app/src/main/java/com/example/donatr/summary/MoreInfoDialog.kt
@@ -2,8 +2,10 @@ package com.example.donatr.summary
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
 import com.example.donatr.R
 import com.example.donatr.data.Charity
 import com.example.donatr.databinding.MoreInfoDialogBinding
@@ -27,6 +29,9 @@ class MoreInfoDialog (private val workingCharity : Charity) : DialogFragment() {
     }
 
     private fun updateView() {
+        Glide.with(requireContext()).load(workingCharity.imgUrl).into(
+            infoViewBinding.imageView
+        )
         infoViewBinding.tvCharityName.text = workingCharity.title
         infoViewBinding.tvType.text = workingCharity.type
         infoViewBinding.tvLocation.text = "New York City"
